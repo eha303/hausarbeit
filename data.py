8795c1410cdad59abf6b881e5060bf2e5e0bee18
Erik Hellbing <eha@gmx.net>
2024-03-10 19:54:53 +0100

    moved the file loading functions to the dataset-class, so the dataset only needs a filename in constructor instead of an already loaded dataframa

--- data.py
+++ data.py
@@ -3,13 +3,43 @@ import traceback
 from datetime import datetime
 from matplotlib import pyplot as plt
 from matplotlib import style
+import pandas as pd
+from sys import exc_info
+
+
+# does not need to be a class-method as it is static
+def load_data_from_file(filename):
+    try:
+        # try to load data into dataframe and return it
+        dat: pd.DataFrame = pd.read_csv(filename)
+        return dat
+
+    except FileNotFoundError:
+        now = datetime.now().strftime("%d-%m-%Y %I:%M:%S %p")
+        exception_type, exception_value, exception_traceback = exc_info()
+        file_name, line_number, procedure_name, line_code \
+            = traceback.extract_tb(exception_traceback)[-1]
+        # get Logging-Instance from Main Scope
+        logger = logging.getLogger('__main__')
+        logger.error("Exception Datetime: %s", now)
+        logger.error("Exception Type: %s", exception_type)
+        logger.error("Exception Value: %s", exception_value)
+        logger.error("File Name: %s", file_name)
+        logger.error("Line Number: %d", line_number)
+        logger.error("Procedure Name: %s", procedure_name)
+        logger.error("Line Code: %s", line_code)
+        # return None to make clear no data has been loaded
+        return None
 
 
 class DataSet:
-    def __init__(self, name, dataframe):
+    def __init__(self, name, filename):
         self.name = name
-        self.dataframe = dataframe
-        self.number_of_columns = 4
+        self.filename = filename
+        self.dataframe = load_data_from_file(filename)
+        if self.dataframe is None:
+            print(filename + ' not found. No Data has been loaded.')
+            del self
 
     def visualize_data(self, x, y):
         x_list = self.dataframe[x].tolist()
@@ -24,8 +54,8 @@ class DataSet:
 
 
 class DataSetWithDatabaseFunctions(DataSet):
-    def __init__(self, name, dataframe):
-        DataSet.__init__(self, name, dataframe)
+    def __init__(self, name, filename):
+        DataSet.__init__(self, name, filename)
         self.database_success = True
 
     def write_to_database(self, engine):
--- main.py
+++ main.py
@@ -1,33 +1,6 @@
-import traceback
 import logging
-from datetime import datetime
-from sys import exc_info
-import pandas as pd
 from database import DataBase
 from data import DataSetWithDatabaseFunctions
-
-def load_data_from_file(filename):
-    try:
-        # try to load data into dataframe and return it
-        dat: pd.DataFrame = pd.read_csv(filename)
-        return dat
-
-    except FileNotFoundError:
-        now = datetime.now().strftime("%d-%m-%Y %I:%M:%S %p")
-        exception_type, exception_value, exception_traceback = exc_info()
-        file_name, line_number, procedure_name, line_code \
-            = traceback.extract_tb(exception_traceback)[-1]
-        # get Logging-Instance from Main Scope
-        logger = logging.getLogger('__main__')
-        logger.error("Exception Datetime: %s", now)
-        logger.error("Exception Type: %s", exception_type)
-        logger.error("Exception Value: %s", exception_value)
-        logger.error("File Name: %s", file_name)
-        logger.error("Line Number: %d", line_number)
-        logger.error("Procedure Name: %s", procedure_name)
-        logger.error("Line Code: %s", line_code)
-        # return None to make clear no data has been loaded
-        return None
 
 
 if __name__ == '__main__':
@@ -35,43 +8,17 @@ if __name__ == '__main__':
 
     # configure logging
     logging.basicConfig(filename="error.log", filemode="a")
+    test_data_set = DataSetWithDatabaseFunctions('TestData', 'test.csv')
+    ideal_data_set = DataSetWithDatabaseFunctions('IdealData', 'ideal.csv')
+    train_data_set = DataSetWithDatabaseFunctions('TrainData', 'train.csv')
 
-    test_data = load_data_from_file('test.csv')
-    if test_data is None:
-        print("Testdata not loaded. File test.csv not found.")
-    else:
-        # create new object from class DataSet
-        test_data_set = DataSetWithDatabaseFunctions('TestData', test_data)
-        # set the correct number of columns for this dataframe
-        test_data_set.number_of_columns = len(test_data.columns)
-        print("Testdata loaded.")
-    ideal_data = load_data_from_file('ideal.csv')
-    if ideal_data is None:
-        print("Ideal Data not loaded. File ideal.csv not found.")
-    else:
-        # create new object from class DataSet
-        ideal_data_set = DataSetWithDatabaseFunctions('IdealData', ideal_data)
-        # set the correct number of columns for this dataframe
-        ideal_data_set.number_of_columns = len(ideal_data.columns)
-        print("Ideal Data loaded.")
-    train_data = load_data_from_file('train.csv')
-    if train_data is None:
-        print("Train Data not loaded. File train.csv not found.")
-    else:
-        # create new object from class DataSet
-        train_data_set = DataSetWithDatabaseFunctions('TrainData', train_data)
-        # set the correct number of columns for this dataframe
-        train_data_set.number_of_columns = len(train_data.columns)
-        print("Train Data loaded.")
-    ideal_db_success = my_db.create_table_from_dataframe("ideal_data",
-                                                         ideal_data)
+    ideal_db_success = ideal_data_set.write_to_database(my_db.engine)
     if ideal_db_success:
         print("Ideal Data stored in Database.")
     else:
         print("Ideal Data NOT stored in Database. Table already exists.\
         Probably you ran the program already and tables have been created.")
-    train_db_success = my_db.create_table_from_dataframe("train_data",
-                                                         train_data)
+    train_db_success = train_data_set.write_to_database(my_db.engine)
     if train_db_success:
         print("Train Data stored in Database.")
     else:
